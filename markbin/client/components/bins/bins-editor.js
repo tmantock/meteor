470e1ceb26fc98915088c4fcbeab4bb35f72b71e
Tevin Mantock <tmantock@gmail.com>
2016-08-10 21:41:59 -0700

    markdown editor added

---
+++ markbin/client/components/bins/bins-editor.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';
+
+class BinsEditor extends Component {
+  onEditorChange(content) {
+    Meteor.call('bins.update', this.props.bin, content)
+  }
+
+  render(){
+    return (
+      <div className="col-sm-8">
+        <h5>Input</h5>
+        <CodeMirror onChange={this.onEditorChange.bind(this)} value={this.props.bin.content} options={{ mode: 'markdown', lineNumbers: true }} />
+      </div>
+    );
+  }
+}
+
+export default BinsEditor;
--- markbin/client/main.js
+++ markbin/client/main.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
 import { Bins } from '../imports/collections/bins'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import App from './components/app';
+import BinsMain from './components/bins/bins-main';
+import BinsList from './components/bins/bins-list';
+
+const routes = (
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={BinsList} />
+        <Route path="bins/:binId" component={BinsMain} />
+      </Route>
+    </Router>
+);
 
 Meteor.startup(()=>{
-  ReactDOM.render(<App />, document.querySelector('.render-target'));
+  ReactDOM.render(routes, document.querySelector('.render-target'));
 })
